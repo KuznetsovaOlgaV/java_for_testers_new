d58958ea3fcb5ba4cff9ef3907ee0965b8779074
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-09 23:19:36 +0300

    Лекция 1.9. Record: особый вид классов

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,26 +1,14 @@
 package ru.stqa.geometry.figures;
 
-public class Square {
+public record Square(double side) {
 
-    private double side;
 
-    public Square (double side) {
-        this.side = side;
-    }
 
     public static void printSquareArea(Square s){
         String text = String.format("Площадь квадрата со стороной %f= %f", s.side, s.area());
         System.out.println(text);
     }
-
-//    public static double area(double a) {
-//        return a * a;
-//    }
-
-//    public static double perimeter(double a) {
-//        return 4 *a;
-//    }
-
+    
     public double area() {
         return this.side * this.side;
     }
