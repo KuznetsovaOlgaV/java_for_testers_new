416497c6be973b251b36b45810e3fa78d6ce3b3e
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-14 14:36:36 +0300

    Треугольник_тесты

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -5,21 +5,25 @@ import static java.lang.Math.sqrt;
 public class Triangle {
 
     public static void printAreaTriangle(double a, double b, double c) {
-        var text = String.format("площадь треугольника со сторонами %f и %f и %f =%f", a, b, c, areaTriangle(a, b, c));
+        var text = String.format("площадь треугольника со сторонами %f и %f и %f =%f", a, b, c, area(a, b, c));
         System.out.println(text);
     }
 
-    private static double areaTriangle(double a, double b, double c) {
+    public static double area(double a, double b, double c) {
         return sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
     }
 
-    public static void printPerimetrTriangle(double a, double b, double c){
-       var text = String.format("периметр треугольника со сторонами %f и %f и %f =%f", a, b, c, perimetrTriangle(a, b, c));
-        System.out.println(text);
-    }
-
-    private static double perimetrTriangle(double a, double b, double c) {
+    public static double perimetr(double a, double b, double c) {
         return a + b + c;
     }
+//
+//    public static void printPerimetrTriangle(double a, double b, double c){
+//       var text = String.format("периметр треугольника со сторонами %f и %f и %f =%f", a, b, c, perimetrTriangle(a, b, c));
+//        System.out.println(text);
+//    }
+//
+//    private static double perimetrTriangle(double a, double b, double c) {
+//        return a + b + c;
+//    }
 }
 
