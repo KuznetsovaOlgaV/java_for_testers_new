3036ba305e3034211a71f5e9c91965a418590034
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-23 23:11:02 +0300

    Лекция 2.4. Сравнение объектов (метод equals)

--- sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTests.java
@@ -10,8 +10,23 @@ public class RectangleTests {
         try {
             new Rectangle(-5.0, 3.0);
             Assertions.fail();
-        } catch(IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
             //OK
         }
     }
+
+    @Test
+    void testEquality() {
+        var r1 = new Rectangle(5.0, 4.0);
+        var r2 = new Rectangle(5.0, 4.0);
+        Assertions.assertEquals(r1, r2);
+    }
+
+    @Test
+    void testEquality2() {
+        var r1 = new Rectangle(5.0, 4.0);
+        var r2 = new Rectangle(4.0, 5.0);
+        Assertions.assertEquals(r1, r2);
+    }
+
 }
