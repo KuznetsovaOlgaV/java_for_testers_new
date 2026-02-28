fb0995946828f64e21bc66aad134367a0404cea1
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-28 09:59:05 +0300

    Contacts_создание+доработано

--- addressbook_web_tests/src/test/java/ContactsCreationTests.java
+++ addressbook_web_tests/src/test/java/ContactsCreationTests.java
@@ -1,40 +1,32 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ContactsCreationTests {
-    private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
+    private static WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        driver = new FirefoxDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(1811, 1077));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
     }
 
     @Test
-    public void test() {
-        driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1811, 1077));
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        driver.findElement(By.linkText("add new")).click();
+    public void canCreateContacts() {
+        if (!isElementPresent(By.name("Number of results:"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
         driver.findElement(By.name("theform")).click();
         driver.findElement(By.name("firstname")).click();
         driver.findElement(By.name("firstname")).sendKeys("first name");
@@ -48,6 +40,35 @@ public class ContactsCreationTests {
         driver.findElement(By.name("email")).sendKeys("e-mail");
         driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
         driver.findElement(By.linkText("home page")).click();
-        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+    @Test
+    public void canCreateContactsWithEmptyFirstName() {
+        if (!isElementPresent(By.name("Number of results:"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
+        driver.findElement(By.linkText("add new")).click();
+        driver.findElement(By.name("theform")).click();
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).sendKeys("");
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).sendKeys("");
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).sendKeys("");
+        driver.findElement(By.name("mobile")).click();
+        driver.findElement(By.name("mobile")).sendKeys("");
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys("");
+        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+        driver.findElement(By.linkText("home page")).click();
     }
 }
